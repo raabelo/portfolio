e44e6c6b0f3d3fce749703f78cf92437dde849ed
raabelo <raabelo@hotmail.com>
2023-02-18 21:17:58 -0300

    att 18022023 ; about v0 ; skills v0

---
+++ src/assets/docs/skills.js
@@ -0,0 +1,89 @@
+import react from "../../assets/imgs/react.png"
+import node from "../../assets/imgs/node.png"
+import html from "../../assets/imgs/html.png"
+import css from "../../assets/imgs/css.png"
+import js from "../../assets/imgs/js.png"
+import mongo from "../../assets/imgs/mongo.png"
+import sql from "../../assets/imgs/sql.png"
+import csharp from "../../assets/imgs/csharp.png"
+import java from "../../assets/imgs/java.png"
+import python from "../../assets/imgs/python.png"
+import figma from "../../assets/imgs/figma.png"
+import illustrator from "../../assets/imgs/illustrator.png"
+import photoshop from "../../assets/imgs/photoshop.png"
+import redux from "../../assets/imgs/redux.png"
+import sass from "../../assets/imgs/sass.png"
+import firebase from "../../assets/imgs/firebase.png"
+
+const skills = [
+    {
+        name: "React.js",
+        icon: react,
+    },
+    {
+        name: "Node.js",
+        icon: node,
+    },
+    {
+        name: "HTML",
+        icon: html,
+    },
+    {
+        name: "CSS",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: js,
+    },
+    {
+        name: "MongoDB",
+        icon: mongo,
+    },
+    {
+        name: "MySQL",
+        icon: sql,
+    },
+    {
+        name: "C#",
+        icon: csharp,
+    },
+    {
+        name: "Java",
+        icon: java,
+    },
+    {
+        name: "Python",
+        icon: python,
+    },
+    {
+        name: "Figma",
+        icon: figma,
+    },
+    {
+        name: "Illustrator",
+        icon: illustrator,
+    },
+    {
+        name: "Photoshop",
+        icon: photoshop,
+    },
+    {
+        name: "React Native",
+        icon: react,
+    },
+    {
+        name: "Redux",
+        icon: redux,
+    },
+    {
+        name: "Sass",
+        icon: sass,
+    },
+    {
+        name: "Firebase",
+        icon: firebase,
+    },
+];
+
+export default skills;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,10 +8,14 @@ import "./styles.css";
 // assets
 import lightLogo from "../../assets/logos/light.png";
 import darkLogo from "../../assets/logos/dark.png";
+import skills from "../../assets/docs/skills";
 
 // components
 import SwitchIcon from "../../components/ui/switch-icon";
 import ButtonWord from "../../components/ui/button-word";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const Home = ({ theme = "dark", setTheme }) => {
     const [switchTheme, setSwitchTheme] = useState(true);
@@ -26,11 +30,15 @@ const Home = ({ theme = "dark", setTheme }) => {
 
     return (
         <div className={"body-wrapper theme-" + theme}>
+            <button id="btn-backtotop">
+                <FontAwesomeIcon icon={faArrowUp} />
+            </button>
             <div>
                 <header className={"header-menu"}>
                     <img width={150} src={theme === "dark" ? lightLogo : darkLogo}></img>
                     <div className={"div-wrapper-menu"}>
                         <button className={"theme-" + theme}>Início</button>
+                        <button className={"theme-" + theme}>Sobre mim</button>
                         <button className={"theme-" + theme}>Habilidades</button>
                         <button className={"theme-" + theme}>Projetos</button>
                         <button className={"theme-" + theme}>Contatos</button>
@@ -44,12 +52,12 @@ const Home = ({ theme = "dark", setTheme }) => {
                         <div>
                             <div>
                                 <p>Olá, eu sou o</p>
-                                <p style={{ color: "var(--color-primary)" }}>Fabiano Rabelo {":)"}</p>
+                                <p style={{ color: "var(--color-primary)" }}>Fabiano Rabelo {""}</p>
                                 <p
                                     style={{
                                         color: "var(--color-lightblack)",
                                         fontSize: "var(--font-size-xxx-large)",
-                                        marginTop: 5
+                                        marginTop: 5,
                                     }}
                                 >
                                     Desenvolvedor Full-Stack
@@ -80,8 +88,29 @@ const Home = ({ theme = "dark", setTheme }) => {
                         </div>
                         <div></div>
                     </section>
-                    <section>
-                        <p>testing</p>
+                    <section id="section-about">
+                        <p>Sobre mim</p>
+                        <p>
+                            {/* Desenvolvedor Front-end com experiência em React.js, Next.js, Redux,
+                            JavaScript, TypeScript, Styled Components, Material UI e consumo de APIs
+                            REST. Apaixonado pelo desenvolvimento de componentes reutilizáveis.
+                            Também possui experiência em projetos gerenciados por Metodologias
+                            Ágeis. Formado em Sistemas de Informação e cursando Especialização em
+                            Front-End na Digital House Brasil. */}
+                        </p>
+                    </section>
+                    <section id="section-skills">
+                        <p>Habilidades</p>
+                        <div>
+                            {skills.map((element, i) => {
+                                return (
+                                    <div key={"skills-" + i}>
+                                        <p>{element.name}</p>
+                                        <img src={element.icon} width={50}></img>
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </section>
                 </main>
                 <footer></footer>
